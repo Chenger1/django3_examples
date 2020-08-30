5bde7c47ee6699e50063d0078ece50691a22c4a6
Vladislav <exs2199@gmail.com>
2020-08-30 17:02:24 +0300

    Add post sharing via email

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import post_detail, PostListView
+from .views import post_detail, PostListView, post_share
 
 app_name = 'blog'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          post_detail,
          name='post_detail'),
+    path('<int:post_id>/share/',
+         post_share, name='post_share'),
 ]

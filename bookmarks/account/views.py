b3d4f41173f6bd23e7ef7b7ba2213a6f9431cec2
Vladislav <exs2199@gmail.com>
2020-09-05 20:31:34 +0300

    Add system messages to inform user about operation status

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import LoginForm, RegistrationForm, UserEditForm, ProfileEditForm
 
@@ -67,6 +68,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Your profile updated successfully')
+        else:
+            messages.error(request, 'Some data were invalid. Try again.')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)

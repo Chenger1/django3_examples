33619c863a567e56b6252bb9ffddd49128936096
Vladislav <exs2199@gmail.com>
2020-09-07 18:22:50 +0300

    Add image form

---
+++ bookmarks/images/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import image_create
+
+
+urlpatterns = [
+    path('create/', image_create, name='image_create'),
+]
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .forms import ImageForm
+
+
+@login_required
+def image_create(request):
+    if request.method=='POST':
+        image_form = ImageForm(data=request.POST)
+        if image_form.is_valid():
+            cd = image_form.cleaned_data
+            new_image = ImageForm.save(commit=False)
+            new_image.user = request.user
+            messages.success(request, 'Image has been successfully added')
+            return redirect(new_image.get_absolute_url())
+    else:
+        image_form = ImageForm(data=request.GET)
+
+    return render(request, 'images/image/create.html',
+                  {'section': 'images',
+                   'form': image_form})

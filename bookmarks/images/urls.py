60c49015fe494b3d612dcab6aa6251dffe5421d7
Vladislav <exs2199@gmail.com>
2020-09-08 21:05:21 +0300

    Add view for like/unlike images

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import image_create, image_detail
+from .views import image_create, image_detail, image_like
 
 
 app_name = 'images'
@@ -9,4 +9,5 @@ app_name = 'images'
 urlpatterns = [
     path('create/', image_create, name='image_create'),
     path('detail/<int:id><slug:slug>', image_detail, name='image_detail'),
+    path('like/', image_like, name='like'),
 ]

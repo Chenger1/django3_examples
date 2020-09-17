15dc5707da41e159559800a212ed6f0ac6492508
Vladislav <exs2199@gmail.com>
2020-09-17 21:20:53 +0300

    Add redis to store total_views counter for images

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -3,12 +3,20 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.conf import settings
 
 from common.decorators import ajax_required
+
+import redis
 
 from .forms import ImageForm
 from .models import Image
 from actions.utils import create_actions
+
+
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
 
 
 @login_required
@@ -33,10 +41,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f'image:{image.id}:views')
 
     return render(request, 'images/image/detail.html',
                   {'section': 'images',
-                   'image': image})
+                   'image': image,
+                   'total_views': total_views})
 
 @ajax_required
 @login_required

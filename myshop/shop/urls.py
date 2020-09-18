8707957cc29417d0004b63aa2a98ab68d3b2d05e
Vladislav <exs2199@gmail.com>
2020-09-18 18:40:02 +0300

    Add templates to display products list and product detail

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import product_detail, product_list
 
@@ -11,3 +13,7 @@ urlpatterns = [
     path('/<int:id>/<slug:slug>/', product_detail, name='product_detail'),
     path('<slug:category_slug>/', product_list, name='product_list_by_category'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)

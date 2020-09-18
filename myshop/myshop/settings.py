cc214891c551bf8e22a7ffb4a3777c4ad7a746e3
Vladislav <exs2199@gmail.com>
2020-09-18 18:01:49 +0300

    Add Category and Product models

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop.ShopConfig',
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [

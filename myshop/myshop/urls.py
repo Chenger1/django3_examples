7e9b9abe2a59fc714021aa14f24361678bc4c261
Vladislav <exs2199@gmail.com>
2020-10-30 22:14:35 +0200

    Update settings to work with orders app

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
-    path('shop/', include('shop.urls', namespace='shop'))
+    path('shop/', include('shop.urls', namespace='shop')),
+    path('orders/', include('orders.urls', namespace='orders')),
 ]

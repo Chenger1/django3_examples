ddb18f872ec8f33c32b2ec8b03ec501b05e3eaba
Vladislav <exs2199@gmail.com>
2020-11-01 19:01:12 +0200

    Update Cart object for using coupons

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
 from django.conf import settings
+
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart:
@@ -11,6 +13,7 @@ class Cart:
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        self.coupon_id = self.session.get('coupon_id')
 
     def add(self, product, quantity=1, override_quantity=False):
         """Add a product to the cart or update uts quantity"""
@@ -46,6 +49,24 @@ class Cart:
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
             yield item
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) \
+                    * self.get_total_price()
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
 
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())

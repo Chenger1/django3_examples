5937d22c60891b7f4f2d0b89cffd112805257bc9
Vladislav <exs2199@gmail.com>
2020-11-03 18:28:16 +0200

    Add language switching to main page

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en'
-LANGUAGE = (
+LANGUAGES = (
     ('en', _('English')),
     ('ru', _('Russian')),
 )

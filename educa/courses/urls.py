9841738358903c69e1ea3b0378431b333aa36a3f
Vladislav <exs2199@gmail.com>
2020-11-04 17:31:07 +0200

    Update urls

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
     path('<pk>/delete/',
          views.CourseDeleteView.as_view(),
          name='course_delete'),
+    path('<pk>/module/', views.CourseModuleUpdateView.as_view(),
+         name='course_module_update'),
 ]

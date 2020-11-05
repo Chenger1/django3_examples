63a87db44031535b8b6711acb28ed8e55c20d5b4
Vladislav <exs2199@gmail.com>
2020-11-05 17:58:03 +0200

    Add page to display courses

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -13,6 +13,8 @@ from .config import secret_key
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')

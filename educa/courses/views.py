d23cf9602ab8ef7d045dfc5461381af9382f0f2f
Vladislav <exs2199@gmail.com>
2020-11-07 10:56:21 +0200

    Add cashing for view

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import redirect, get_object_or_404
 from django.forms.models import modelform_factory
 from django.apps import apps
 from django.db.models import Count
+from django.core.cache import cache
 
 from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
@@ -190,9 +191,12 @@ class CourseListView(TemplateResponseMixin, View):
     template_name = 'course/list.html'
 
     def get(self, request, subject=None):
-        subjects = Subject.objects.annotate(
-            total_courses=Count('courses')
-        )
+        subjects = cache.get('all_subjects')
+        if not subjects:
+            subjects = Subject.objects.annotate(
+                total_courses=Count('courses')
+            )
+            cache.set('all_subjects', subjects)
         courses = Course.objects.annotate(
             total_modules=Count('modules')
         )

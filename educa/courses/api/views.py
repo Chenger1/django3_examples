8d6a3c69b9f1b7b13236f253db71649579a4979c
Vladislav <exs2199@gmail.com>
2020-11-07 13:03:50 +0200

    Add listing all courses in API and use router to set urls automatically

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -3,13 +3,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
 from django.shortcuts import get_object_or_404
 
 
 from ..models import Subject, Course
 
-from .serializers import SubjectSerializer
+from .serializers import SubjectSerializer, CourseSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -30,3 +31,8 @@ class CourseEnrollView(APIView):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer

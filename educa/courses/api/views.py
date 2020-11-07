5f0be22d58b1979de2fbd6fd4679ee9e395154ef
Vladislav <exs2199@gmail.com>
2020-11-07 12:57:38 +0200

    Add auth class to view

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
 
 from django.shortcuts import get_object_or_404
 
@@ -21,6 +22,8 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 
 class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
